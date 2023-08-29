c3687742205ead337fcac1e2490f8c3f66b73061
Greg Geer <gregory.geer@gmail.com>
2023-08-29 14:17:30 -0400

    Finished section 1

--- frontend-training/greg/complete-javascript-course-master/01-Fundamentals-Part-1/starter/js/script.js
+++ frontend-training/greg/complete-javascript-course-master/01-Fundamentals-Part-1/starter/js/script.js
@@ -1,4 +1,22 @@
-let js = "amazing";
-if (js === "amazing") alert("Its amazing!");
-40 + 8 + 12 + 212;
-console.log(40 + 8 + 12 + 212);
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+if (shouldDrive) {
+  console.log("Sarah is able to drive");
+} else {
+  console.log("Sarah is not able to drive");
+}
+
+const isTired = true;
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is ble to drive");
+} else {
+  console.log("Someone else should drive..");
+}

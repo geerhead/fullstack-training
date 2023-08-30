d02d6ed524a6c327f6b16c20a2d25e00d8a84a1f
Greg Geer <gregory.geer@gmail.com>
2023-08-30 13:40:42 -0400

    Pushing

---
+++ frontend-training/greg/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+  calcAge: function () {
+    return 2037 - this.birthYear;
+  },
+};
+
+console.log(jonas.calcAge());

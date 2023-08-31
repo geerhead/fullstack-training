39981da5cfb022ee6872f10c961d89bcd10a5ece
Greg Geer <gregory.geer@gmail.com>
2023-08-31 06:30:59 -0400

    Finished 73, moving onto implementing game logic

--- frontend-training/greg/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,8 +1,17 @@
 'use strict';
+//
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Hello World!!';
+// document.querySelector('.number').textContent = '13';
+// document.querySelector('.score').textContent = '20';
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Hello World!!';
-document.querySelector('.number').textContent = '13';
-document.querySelector('.score').textContent = '20';
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});

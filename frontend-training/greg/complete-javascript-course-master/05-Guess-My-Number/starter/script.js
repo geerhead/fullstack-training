9f9b3dfcfee4e40edca9127c9a0bd3430bcd5d1f
Greg Geer <gregory.geer@gmail.com>
2023-08-31 06:16:08 -0400

    Finished section 72, onto 73 to handle click events

--- frontend-training/greg/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Hello World!!';
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '20';
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);

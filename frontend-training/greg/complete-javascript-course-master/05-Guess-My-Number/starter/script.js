8723ce614aa7d2f91e2a6c80fd5de2d92e75bdaa
Greg Geer <gregory.geer@gmail.com>
2023-08-31 11:14:49 -0400

    Finished 78, onto next project

--- frontend-training/greg/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -7,10 +7,11 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = '?';
 let originalMessage = document.querySelector('.message').textContent;
-let score = document.querySelector('.score').textContent;
+let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +28,10 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('body').style.backgroundColor = 'green';
+    if (score > highScore) {
+      highScore = score;
+    }
+    document.querySelector('.highscore').textContent = highScore;
   } else if (score > 1) {
     // When number is too high
     if (guess > secretNumber) {
@@ -54,6 +59,8 @@ document.querySelector('.again').addEventListener('click', () => {
   document.querySelector('.score').textContent = '20';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
 
 // Coding Challenge #1

88b496ebb92df99d7c88fcd19bd54724c4c38af8
Greg Geer <gregory.geer@gmail.com>
2023-09-26 06:38:24 -0400

    Section 191 finish

--- frontend-training/greg/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -139,3 +139,41 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// const h1 = document.querySelector('h1');
+//
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading');
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+//
+// h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading');
+// };
+
+// rgb(255,255,255)
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+//
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+//
+//   //Stop event propagation.
+//
+//   e.stopPropagation();
+// });
+//
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('CONTAINER', e.target, e.currentTarget);
+// });
+//
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('NAV', e.target, e.currentTarget);
+// });

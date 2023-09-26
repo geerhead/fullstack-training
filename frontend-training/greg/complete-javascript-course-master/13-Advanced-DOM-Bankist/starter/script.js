52540d4ae71c965115c8c41a4d005df1160a28fc
Greg Geer <gregory.geer@gmail.com>
2023-09-26 08:45:42 -0400

    Passing arguments into event handlers, lecture 195 complete

--- frontend-training/greg/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -2,7 +2,7 @@
 
 ///////////////////////////////////////
 // Modal window
-
+const nav = document.querySelector('.nav')
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -110,6 +110,26 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+
+const handleHover = function(e) {
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    })
+    logo.style.opacity = this;
+
+  }
+}
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1))
 
 //Bad practice, use event delegation
 // tabs.forEach(t => t.addEventListener('click', () => {

b1841db7495e87e73333b1fe094ef05b212255ba
Greg Geer <gregory.geer@gmail.com>
2023-10-13 21:10:09 -0400

    Finished lecture 209, prototypes

--- frontend-training/greg/complete-javascript-course-master/14-OOP/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  // Never do this - never put methods in function constructors
+  // this.calcAge = function() {
+  //     console.log(2037 - this.birthYear)
+  // }
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// Behind the scenes steps
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} is linked to prototype
+// 4. function automatically returns the {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+//.prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));

26ee39b6bb3694dc669e250724bf360d549287fd
Greg Geer <gregory.geer@gmail.com>
2023-10-15 08:34:22 -0400

    Finished Getters/Setters L214

--- frontend-training/greg/complete-javascript-course-master/14-OOP/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/14-OOP/starter/script.js
@@ -116,34 +116,67 @@ GOOD LUCK 😀
 
 // class declaration
 
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+//
+//   // Methods will be added to the prototype of the class since it's outside the constructor
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+//
+//   greet() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
+//
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+//
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
+//
+//   get fullName() {
+//     return this._fullName;
+//   }
+// }
 
-  // Methods will be added to the prototype of the class since it's outside the constructor
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
+// const jessica = new PersonCl('Jessica Davis', 1996);
+// console.log(jessica.age);
+// console.log(jessica);
+// console.log(jessica.fullName);
+// jessica.calcAge();
+// console.log(jessica.__proto__ === PersonCl.prototype);
 
-  greet() {
-    console.log(`Hey ${this.firstName}`);
-  }
-}
-
-const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
-jessica.calcAge();
-console.log(jessica.__proto__ === PersonCl.prototype);
-
+// const walter = new PersonCl('Walter White', 1965);
 // PersonCl.prototype.greet = function () {
 //   console.log(`Hey ${this.firstName}`);
 // };
 
-jessica.greet();
+// jessica.greet();
 
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizens
 // 3. Classes are executed in strict mode
 // Personal preference for using constructor functions
+
+// const account = {
+//   owner: 'jonas',
+//   movements: [200, 530, 120, 300],
+//
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+//
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+//
+// console.log(account.latest);
+// account.latest = 3000;
+// console.log(account.latest);

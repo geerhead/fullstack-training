c87f23214591621d9a47d628363f76c27a4db82a
Greg Geer <gregory.geer@gmail.com>
2023-10-22 10:11:22 -0400

    Finished L221

--- frontend-training/greg/complete-javascript-course-master/14-OOP/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/14-OOP/starter/script.js
@@ -124,41 +124,41 @@ GOOD LUCK 😀
 
 // Person Class declaration
 
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
-
-  // Methods will be added to the prototype of the class since it's outside the constructor
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
-
-  greet() {
-    console.log(`Hey ${this.fullName}`);
-  }
-
-  get age() {
-    return 2037 - this.birthYear;
-  }
-
-  set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a full name!`);
-  }
-
-  get fullName() {
-    return this._fullName;
-  }
-
-  // Static method
-
-  static hey() {
-    console.log('Hey there!!');
-    console.log('this');
-  }
-}
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+//
+//   // Methods will be added to the prototype of the class since it's outside the constructor
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+//
+//   greet() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
+//
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+//
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
+//
+//   get fullName() {
+//     return this._fullName;
+//   }
+//
+//   // Static method
+//
+//   static hey() {
+//     console.log('Hey there!!');
+//     console.log('this');
+//   }
+// }
 
 // PersonCl.hey();
 
@@ -348,29 +348,57 @@ GOOD LUCK */
 // tesla.accelerate();
 // console.log(tesla);
 
-class StudentCl extends PersonCl {
-  constructor(fullName, birthYear, course) {
-    //Always needs to happen first!
-    super(fullName, birthYear); //Sets up this keyword
-    this.course = course;
-  }
+// class StudentCl extends PersonCl {
+//   constructor(fullName, birthYear, course) {
+//     //Always needs to happen first!
+//     super(fullName, birthYear); //Sets up this keyword
+//     this.course = course;
+//   }
+//
+//   introduce() {
+//     console.log(
+//       `Hey my name ${this.fullName} and I am studying ${this.course}`
+//     );
+//   }
+//
+//   calcAge() {
+//     console.log(
+//       `I'm ${2037 - this.birthYear} years old, but I feel more like ${
+//         2037 - this.birthYear + 10
+//       }`
+//     );
+//   }
+// }
+//
+// const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
+// console.log(martha);
 
-  introduce() {
-    console.log(
-      `Hey my name ${this.fullName} and I am studying ${this.course}`
-    );
-  }
-
+const PersonProto = {
   calcAge() {
-    console.log(
-      `I'm ${2037 - this.birthYear} years old, but I feel more like ${
-        2037 - this.birthYear + 10
-      }`
-    );
-  }
-}
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
 
-const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
-martha.introduce();
-martha.calcAge();
-console.log(martha);
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();

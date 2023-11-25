ca1df01509c38b00ff20228b9e23ee697550ff76
Greg Geer <gregory.geer@gmail.com>
2023-11-25 12:17:41 -0500

    Setup the markers with a clas and to not close, section 234 done

--- frontend-training/greg/complete-javascript-course-master/15-Mapty/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/15-Mapty/starter/script.js
@@ -21,16 +21,30 @@ if (navigator.geolocation)
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 15);
+      // console.log(map);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');

3ae16595cbda25ad3fa664f6995cf4822ed20e31
Greg Geer <gregory.geer@gmail.com>
2023-11-26 12:06:39 -0500

    Finished up to section 238

--- frontend-training/greg/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -1,6 +1,45 @@
 'use strict';
-
+// https://restcountries.com/v3.1/name/deutschland
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    let currency = Object.keys(data.currencies)[0];
+    let language = Object.keys(data.languages)[0];
+    console.log(currency);
+    console.log(language);
+    const html = `
+  <article class="country">
+          <img class="country__img" src=${data.flags.svg} />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${Object.keys(
+              data.currencies
+            )}</p>
+          </div>
+        </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('cn');
+getCountryData('germany');

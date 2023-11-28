09221cd2715f4c72ec8df3f76938c995cd08b05a
Greg Geer <gregory.geer@gmail.com>
2023-11-28 15:38:46 -0500

    Top level awaits complete

--- frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -15,7 +15,28 @@
 // console.log(ShoppingCart.totalQuantity);
 
 // Default import
-// import sc, { cart } from './shoppingCart.js';
-//
-// sc('pizza', 2);
-// console.log(cart);
+import sc, { cart } from './shoppingCart.js';
+
+sc('pizza', 2);
+console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+// Top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,11 @@
 // Exporting module
 console.log('Exporting Module');
 
+// Blocking code
+console.log('Start fetching users');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('Finished fetching users');
+
 const shippingCost = 10;
 export const cart = [];
 

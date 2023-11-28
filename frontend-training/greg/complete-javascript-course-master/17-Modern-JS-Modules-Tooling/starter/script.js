ab0b681f0887c688efea54daa595a325069b709e
Greg Geer <gregory.geer@gmail.com>
2023-11-28 15:25:09 -0500

    finished 272, understanding imports and exports

---
+++ frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,21 @@
+// Import module
+// import {
+//   addToCart,
+//   totalPrice as tp,
+//   totalQuantity as tq,
+// } from './shoppingCart.js';
+
+// addToCart('tampons', 1);
+// console.log(tp);
+// console.log(tq);
+
+// import * as ShoppingCart from './shoppingCart.js';
+//
+// console.log('Importing Module');
+// console.log(ShoppingCart.totalQuantity);
+
+// Default import
+// import sc, { cart } from './shoppingCart.js';
+//
+// sc('pizza', 2);
+// console.log(cart);
---
+++ frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,20 @@
+// Exporting module
+console.log('Exporting Module');
+
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}

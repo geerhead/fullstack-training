834044c21be98fac2f866ba0f6f11b9c7deb81ee
Greg Geer <gregory.geer@gmail.com>
2023-11-28 15:57:43 -0500

    Finished 274

--- frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -40,3 +40,31 @@ const lastPost = getLastPost();
 // Top level await
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('pizza', 5);
+ShoppingCart2.addToCart('donut', 10);
+console.log(...ShoppingCart2.cart);

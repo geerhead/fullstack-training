7f9d770bc360278ac485bb975f9d6fdbe44732b7
Greg Geer <gregory.geer@gmail.com>
2023-11-29 08:09:24 -0500

    Adding at point where having issues

--- frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ frontend-training/greg/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -15,10 +15,10 @@
 // console.log(ShoppingCart.totalQuantity);
 
 // Default import
-import sc, { cart } from './shoppingCart.js';
-
-sc('pizza', 2);
-console.log(cart);
+// import sc, { cart } from './shoppingCart.js';
+//
+// sc('pizza', 2);
+// console.log(cart);
 
 // console.log('Start fetching');
 // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
@@ -26,45 +26,68 @@ console.log(cart);
 // console.log(data);
 // console.log('Something');
 
-const getLastPost = async function () {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  const data = await res.json();
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+// const getLastPost = async function () {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   const data = await res.json();
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
+// const lastPost = getLastPost();
 
 // Not very clean
 // lastPost.then(last => console.log(last));
 
 // Top level await
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+//
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+//
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+//
+//   const orderStock = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+//
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+//
+// ShoppingCart2.addToCart('pizza', 5);
+// ShoppingCart2.addToCart('donut', 10);
+// console.log(...ShoppingCart2.cart);
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// export.addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+//
+// // Import
+// const { addToCart } = require('./shoppingCart.js')
 
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`);
-  };
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
-  const orderStock = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
-
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
-
-ShoppingCart2.addToCart('pizza', 5);
-ShoppingCart2.addToCart('donut', 10);
-console.log(...ShoppingCart2.cart);
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+console.log(state);
+const stateClone = Object.assign({}, state);
+console.log(stateClone);
+state.user.loggedIn = false;
+console.log(stateClone);

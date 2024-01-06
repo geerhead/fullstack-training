3c11360e91e2950aa1ce640e4d749a9a4ee824ab
Greg Geer <gregory.geer@gmail.com>
2024-01-06 18:08:26 -0500

    Adding profile card WIP

--- frontend-training/greg/ultimate-react-course-main/03-pizza-menu/code-challenge/profile-card/src/index.js
+++ frontend-training/greg/ultimate-react-course-main/03-pizza-menu/code-challenge/profile-card/src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <ProfileCard />;
+}
+
+function ProfileCard() {
+  return (
+    <div className={"profile-container"}>
+      <ProfilePhoto
+        img_src={"profile_pics/Greg.jpeg"}
+        description={"Picture of Greg Geer"}
+      />
+      <ProfileInfo
+        name={"Greg Geer"}
+        description={
+          "DevOps Engineer turned Full-stack to better understand the SLDC. I enjoy everything technology and believe" +
+          "in continued learning. I enjoy League of Legends TFT and old school MMORPGs like Everquest P99"
+        }
+      />
+      <ProfileSkills />
+    </div>
+  );
+}
+
+function ProfilePhoto(props) {
+  return (
+    <img
+      className={"profile_photo"}
+      src={props.img_src}
+      alt={props.description}
+    />
+  );
+}
+
+function ProfileInfo(props) {
+  return (
+    <div className={"profile_info"}>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+function ProfileSkills(props) {
+  return (
+    <div>
+      <span className={"skills_span"}>HTML+CSS</span>
+    </div>
+  );
+}
+
+// React V18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

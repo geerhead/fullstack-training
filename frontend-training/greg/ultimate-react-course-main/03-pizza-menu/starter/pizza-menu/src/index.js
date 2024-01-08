b7faf8e0bd7b1d2ac4d4ff8d73db7e9387136239
Greg Geer <gregory.geer@gmail.com>
2024-01-07 19:59:41 -0500

    destructred props

--- frontend-training/greg/ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
+++ frontend-training/greg/ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
@@ -100,16 +100,17 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  // console.log(props);
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+
+  if (pizzaObj.soldOut) return null;
   return (
     <li className={"pizza"}>
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
@@ -128,7 +129,7 @@ function Footer() {
   return (
     <footer className={"footer"}>
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -138,11 +139,12 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className={"order"}>
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order online
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
       </p>
       <button className={"btn"}>Order</button>
     </div>

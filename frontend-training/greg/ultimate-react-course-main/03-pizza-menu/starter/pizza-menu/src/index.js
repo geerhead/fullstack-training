785912ffca26f9e5131998371207a6a498e9b197
Greg Geer <gregory.geer@gmail.com>
2024-01-07 20:17:08 -0500

    Added sold-out class based on ternary oeprator

--- frontend-training/greg/ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
+++ frontend-training/greg/ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
@@ -109,14 +109,15 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
+
   return (
-    <li className={"pizza"}>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );

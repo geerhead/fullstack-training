3a4983315b8a12ce27c68985f32764a6b38fca69
Greg Geer <gregory.geer@gmail.com>
2024-01-07 20:05:41 -0500

    Playing with React Fragments

--- frontend-training/greg/ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
+++ frontend-training/greg/ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
@@ -75,11 +75,17 @@ function Menu() {
     <main className={"menu"}>
       <h2>Our Menu</h2>
       {numPizzas > 0 ? (
-        <ul className={"pizzas"}>
-          {pizzas.map((pizza) => {
-            return <Pizza pizzaObj={pizza} key={pizza.name} />;
-          })}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className={"pizzas"}>
+            {pizzas.map((pizza) => {
+              return <Pizza pizzaObj={pizza} key={pizza.name} />;
+            })}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later</p>
       )}

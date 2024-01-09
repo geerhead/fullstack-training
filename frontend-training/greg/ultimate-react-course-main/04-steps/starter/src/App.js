86f079ad30bab0af018067fcf25449fea69d7ce0
Greg Geer <gregory.geer@gmail.com>
2024-01-08 21:01:39 -0500

    Finished lecture 57, adding dynamic classes to the steps

---
+++ frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App.js
@@ -0,0 +1,30 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const step = 3;
+
+  return (
+    <div className={"steps"}>
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}

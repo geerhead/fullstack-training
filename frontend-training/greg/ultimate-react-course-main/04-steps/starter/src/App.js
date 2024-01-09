c00d06154edb1da3daf7c237c265a42fb492d825
Greg Geer <gregory.geer@gmail.com>
2024-01-08 22:02:41 -0500

    Finished lecture 61

--- frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App.js
+++ frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App.js
@@ -20,9 +20,9 @@ export default function App() {
   return (
     <div className={"steps"}>
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">

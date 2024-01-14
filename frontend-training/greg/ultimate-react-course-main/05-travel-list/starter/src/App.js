54857cc39f57187e7818f34581d944b796f229c4
Greg Geer <gregory.geer@gmail.com>
2024-01-14 13:20:26 -0500

    Finished building and handling submissions on the forms

--- frontend-training/greg/ultimate-react-course-main/05-travel-list/starter/src/App.js
+++ frontend-training/greg/ultimate-react-course-main/05-travel-list/starter/src/App.js
@@ -1,3 +1,72 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 11, packed: true },
+];
+
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>F🏝️ar Away 🧳</h1>;
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder={"Item..."} />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>{!item.packed ? "❌" : "✅"}</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className={"stats"}>
+      <em> 💼You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }

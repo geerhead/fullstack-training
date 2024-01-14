f699a41d3714b16d2b7849c64de4fbd9c5c8e1ab
Greg Geer <gregory.geer@gmail.com>
2024-01-13 21:44:37 -0500

    Almost completed coding challenge

---
+++ frontend-training/greg/ultimate-react-course-main/04-steps/code-challenge/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+  return (
+    <div className={"counter-container"}>
+      <Counter type={"Step"} />
+      <Counter type={"Count"} />
+      <DisplayDate />
+    </div>
+  );
+
+  function minusType(type) {
+    if (type.toLowerCase() === "step") {
+      if (step <= 1) return;
+      setStep((s) => s - 1);
+    } else {
+      setCount((s) => s - 1);
+    }
+  }
+
+  function addType(type) {
+    type.toLowerCase() === "step"
+      ? setStep((s) => s + 1)
+      : setCount((s) => s + 1);
+  }
+
+  function Counter({ type }) {
+    return (
+      <div className={"counter-container"}>
+        <button
+          onClick={() => {
+            minusType(type);
+          }}
+        >
+          -
+        </button>
+        <div>
+          {type}: {type.toLowerCase() === "count" ? count : step}
+        </div>
+        <button
+          onClick={() => {
+            addType(type);
+          }}
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+
+  function DisplayDate() {
+    const date = new Date();
+    const totalDays = count * step;
+    date.setDate(date.getDate() + totalDays);
+    return (
+      <div>
+        {count > 0 ? totalDays : Math.abs(totalDays)}{" "}
+        {Math.abs(totalDays) > 1 ? "days" : "day"} {count < 0 ? "ago" : ""} from
+        today is {date.toDateString()}
+      </div>
+    );
+  }
+}
--- frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App.js
+++ frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App.js
@@ -11,16 +11,18 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (

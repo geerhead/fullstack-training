2234122690d5136e8cd3e3262d122f680ea9cb0e
Greg Geer <gregory.geer@gmail.com>
2024-01-15 16:09:45 -0500

    Separated everything out into separate components

---
+++ frontend-training/greg/ultimate-react-course-main/05-travel-list/starter/src/components/Item.js
@@ -0,0 +1,17 @@
+export default function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
---
+++ frontend-training/greg/ultimate-react-course-main/05-travel-list/starter/src/components/Logo.js
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <h1>🏝 Far Away 🧳</h1>;
+}

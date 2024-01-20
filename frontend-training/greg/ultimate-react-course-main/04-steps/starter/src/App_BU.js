3e915274efa17766038a24c428ffeafff78ef75f
Greg Geer <gregory.geer@gmail.com>
2024-01-19 23:18:58 -0500

    Finished lectures up until accordion v2, lecture 92

--- frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App_BU.js
+++ frontend-training/greg/ultimate-react-course-main/04-steps/starter/src/App_BU.js
@@ -33,42 +33,26 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
           <div className="buttons">
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              <span>⬅️</span>Previous
-            </Button>
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
-              Next<span>➡️</span>
-            </Button>
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, text, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }

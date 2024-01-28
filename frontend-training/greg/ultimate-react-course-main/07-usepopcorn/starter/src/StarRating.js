8fb3cd8129d22ec081f69de32b5a5d6fd532270b
Greg Geer <gregory.geer@gmail.com>
2024-01-28 15:04:09 -0500

    Added the functionality to the stars for changing on hover

--- frontend-training/greg/ultimate-react-course-main/07-usepopcorn/starter/src/StarRating.js
+++ frontend-training/greg/ultimate-react-course-main/07-usepopcorn/starter/src/StarRating.js
@@ -17,6 +17,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -27,13 +28,15 @@ export default function StarRating({ maxRating = 5 }) {
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -45,9 +48,15 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role={"button"} style={starStyle} onClick={onRate}>
+    <span
+      role={"button"}
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"

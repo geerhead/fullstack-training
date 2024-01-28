47471da9a1b8216a794b247712859620fcb4f37c
Greg Geer <gregory.geer@gmail.com>
2024-01-28 14:50:31 -0500

    Created the stars

--- frontend-training/greg/ultimate-react-course-main/07-usepopcorn/starter/src/index.js
+++ frontend-training/greg/ultimate-react-course-main/07-usepopcorn/starter/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+// import "./index.css";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </React.StrictMode>
 );

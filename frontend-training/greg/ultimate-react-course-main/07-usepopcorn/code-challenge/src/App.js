bec89a3c5838f29ae7c54abd9fd13b87b96b3f0a
Greg Geer <gregory.geer@gmail.com>
2024-01-28 22:11:38 -0500

    Could make some enhancemnets, more to think on

--- frontend-training/greg/ultimate-react-course-main/07-usepopcorn/code-challenge/src/App.js
+++ frontend-training/greg/ultimate-react-course-main/07-usepopcorn/code-challenge/src/App.js
@@ -4,13 +4,13 @@ import TextExpander from "./components/TextExpander";
 export default function App() {
   return (
     <div>
-      {/*<TextExpander>*/}
-      {/*  Space travel is the ultimate adventure! Imagine soaring past the stars*/}
-      {/*  and exploring new worlds. It's the stuff of dreams and science fiction,*/}
-      {/*  but believe it or not, space travel is a real thing. Humans and robots*/}
-      {/*  are constantly venturing out into the cosmos to uncover its secrets and*/}
-      {/*  push the boundaries of what's possible.*/}
-      {/*</TextExpander>*/}
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
 
       <TextExpander
         collapsedNumWords={20}
@@ -25,12 +25,12 @@ export default function App() {
         foot on the moon or when rovers were sent to roam around on Mars.
       </TextExpander>
 
-      {/*<TextExpander expanded={true} className="box">*/}
-      {/*  Space missions have given us incredible insights into our universe and*/}
-      {/*  have inspired future generations to keep reaching for the stars. Space*/}
-      {/*  travel is a pretty cool thing to think about. Who knows what we'll*/}
-      {/*  discover next!*/}
-      {/*</TextExpander>*/}
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
     </div>
   );
 }
--- frontend-training/greg/ultimate-react-course-main/07-usepopcorn/code-challenge/src/components/TextExpander.js
+++ frontend-training/greg/ultimate-react-course-main/07-usepopcorn/code-challenge/src/components/TextExpander.js
@@ -1,13 +1,52 @@
 import { useState } from "react";
 
 export default function TextExpander({
-  collapsedNumWords,
-  expandButtonText,
-  collapseButtonText,
   buttonColor,
   children,
+  collapsedNumWords = children.length,
+  collapseButtonText = "Collapse text",
+  expandButtonText = "Show text",
+  expanded,
 }) {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(
+    expanded ? expanded : collapsedNumWords < children.length
+  );
 
-  return <>hi</>;
+  return (
+    <>
+      <div className="box">
+        {isCollapsed ? children.slice(0, -collapsedNumWords) : children}
+        {collapsedNumWords === children.length ? (
+          ""
+        ) : (
+          <Button
+            isCollapsed={isCollapsed}
+            expandButtonText={expandButtonText}
+            collapseButtonText={collapseButtonText}
+            buttonColor={buttonColor}
+            onToggleCollapse={setIsCollapsed}
+          />
+        )}
+      </div>
+    </>
+  );
+}
+
+function Button({
+  expandButtonText = "",
+  collapseButtonText = "",
+  isCollapsed,
+  buttonColor,
+  onToggleCollapse,
+}) {
+  return (
+    <div>
+      <button
+        style={{ color: buttonColor }}
+        onClick={() => onToggleCollapse(!isCollapsed)}
+      >
+        {isCollapsed ? expandButtonText : collapseButtonText}
+      </button>
+    </div>
+  );
 }

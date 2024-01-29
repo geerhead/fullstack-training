994f670bd7e94d3695561fa3fb8dacbbb1988761
Greg Geer <gregory.geer@gmail.com>
2024-01-28 21:35:06 -0500

    Need to refactor this

---
+++ frontend-training/greg/ultimate-react-course-main/07-usepopcorn/code-challenge/src/components/TextExpander.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function TextExpander({
+  collapsedNumWords,
+  expandButtonText,
+  collapseButtonText,
+  buttonColor,
+  children,
+}) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return <>hi</>;
+}

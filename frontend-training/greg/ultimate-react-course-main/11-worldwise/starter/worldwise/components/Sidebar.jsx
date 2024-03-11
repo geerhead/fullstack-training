e4a11be5e0cacb35282fe666216d5ae1686769b9
Greg Geer <gregory.geer@gmail.com>
2024-03-10 21:03:57 -0400

    halfway through section 212

--- frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/Sidebar.jsx
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import {Outlet} from 'react-router-dom'
 import AppNav from "./AppNav.jsx";
 import Logo from './Logo.jsx'
 import styles from './Sidebar.module.css'
@@ -8,7 +9,7 @@ function Sidebar() {
         <div className={styles.sidebar}>
             <Logo />
             <AppNav />
-            <p>List of cities</p>
+            <Outlet />
             <footer className={styles.footer}>
                 <p className={styles.copyright}>
                     &copy; Copyright {new Date().getFullYear()} by WorldWise Inc. All rights reserved.
--- frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/src/App.jsx
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "../pages/AppLayout.jsx";
+// import CityList from "../components/CityList.jsx";
+// import CountryList from "../components/CountryList.jsx";
+import Form from "../components/Form.jsx";
 import Homepage from "../pages/Homepage.jsx";
 import Login from "../pages/Login.jsx";
 import PageNotFound from "../pages/PageNotFound.jsx";
@@ -16,7 +19,13 @@ export default function App() {
         <Route path={"/"} element={<Homepage />}></Route>
         <Route path="product" element={<Product />} />
         <Route path={"pricing"} element={<Pricing />}></Route>
-        <Route path={"app"} element={<AppLayout />}></Route>
+        <Route path={"app"} element={<AppLayout />}>
+          {/*<Route path={"cities"} element={<CityList />}/>*/}
+          <Route path={"cities"} element={ <p>City List</p>}/>
+          {/*<Route path={"countries"} element={<CountryList />}/>*/}
+          <Route path={"countries"} element={<p>Country List</p>}/>
+          <Route path={"form"} element={<Form />}/>
+        </Route>
         <Route path={"/login"} element={<Login />}></Route>
         <Route path={"*"} element={<PageNotFound />} />
       </Routes>

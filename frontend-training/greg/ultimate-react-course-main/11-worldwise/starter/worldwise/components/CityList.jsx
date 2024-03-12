c608bbca002dacd19755465060988740415bf4ef
Greg Geer <gregory.geer@gmail.com>
2024-03-11 21:28:56 -0400

    Added CityList, Cities, and put in guards for data still loading or an empty array

---
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/CityList.jsx
@@ -0,0 +1,21 @@
+import Spinner from "./Spinner.jsx";
+import styles from './CityList.module.css'
+import CityItem from "./CityItem.jsx";
+import Message from "./Message.jsx";
+
+function CityList({cities, isLoading}) {
+
+    if(isLoading) return <Spinner />;
+
+    if (!cities.length) return <Message message={"Add your first city by clicking on a city on the map"} />;
+
+    return (
+        <ul className={styles.cityList}>
+            {cities.map(city => (
+                <CityItem city={city} key={city.id} />
+            ))}
+        </ul>
+    );
+}
+
+export default CityList;
--- frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/src/App.jsx
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/src/App.jsx
@@ -8,9 +8,29 @@ import Login from "../pages/Login.jsx";
 import PageNotFound from "../pages/PageNotFound.jsx";
 import Pricing from "../pages/Pricing.jsx";
 import Product from "../pages/Product.jsx";
+import CityList from "../components/CityList.jsx";
+import { useState, useEffect } from "react";
 
+const BASE_URL = 'http://localhost:8000'
 
 export default function App() {
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(function() {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading the data...")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities();
+  },[]);
   return (
     <BrowserRouter>
       <Routes>
@@ -18,8 +38,8 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path={"pricing"} element={<Pricing />}></Route>
         <Route path={"app"} element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>}/>
-          <Route path={"cities"} element={ <p>City List</p>}/>
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />}/>
+          <Route path={"cities"} element={<CityList  cities={cities} isLoading={isLoading} />}/>
           <Route path={"countries"} element={<p>Country List</p>}/>
           <Route path={"form"} element={<Form />}/>
         </Route>

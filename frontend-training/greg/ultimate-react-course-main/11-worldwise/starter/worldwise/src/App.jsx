60264601ec23aa6f5cec458b375bd0979bfcd365
Greg Geer <gregory.geer@gmail.com>
2024-03-21 19:42:45 -0400

    finished with playing on the Navigate object

--- frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/src/App.jsx
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import AppLayout from "../pages/AppLayout.jsx";
 import CountryList from "../components/CountryList.jsx";
 import Homepage from "../pages/Homepage.jsx";
@@ -38,7 +38,7 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path={"pricing"} element={<Pricing />}></Route>
         <Route path={"app"} element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />}/>
+          <Route index element={<Navigate replace to={'cities'}/>}/>
           <Route path={"cities"} element={<CityList  cities={cities} isLoading={isLoading} />}/>
           <Route path={'cities/:id'} element={<City />}/>
           <Route path={"countries"} element={<CountryList cities={cities} isLoading={isLoading}/>}/>

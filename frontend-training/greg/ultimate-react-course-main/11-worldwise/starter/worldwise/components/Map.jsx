be10f8ecf57ac20b0d78755bff01534487f29186
Greg Geer <gregory.geer@gmail.com>
2024-05-04 14:06:57 -0400

    have the map in a working state but not with the marker working

--- frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/Map.jsx
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/Map.jsx
@@ -1,10 +1,12 @@
 import styles from './Map.module.css'
 import {useNavigate, useSearchParams} from "react-router-dom";
+import {useState} from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
 function Map() {
     const navigate = useNavigate()
-
     const [searchParams, setSearchParams] = useSearchParams();
+    const [mapPosition, setMapPosition] = useState([40,21])
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
 
@@ -13,13 +15,17 @@ function Map() {
         <div className={styles.mapContainer} onClick={() => {
             navigate("form");
         }}>
-            <h1>Map</h1>
-            <h1>
-                Position: {lat}, {lng}
-            </h1>
-            <button onClick={() => {
-                setSearchParams({lat: 23, lng: 50})
-            }}>Change position</button>
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={mapPosition}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     );
 }

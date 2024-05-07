9f71bceb8b7c5bff5bb818e8a5e078e40e23fa2e
Greg Geer <gregory.geer@gmail.com>
2024-05-07 07:14:53 -0400

    Changed the style of the map and added the scroll wheel after everything is working

--- frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/Map.jsx
+++ frontend-training/greg/ultimate-react-course-main/11-worldwise/starter/worldwise/components/Map.jsx
@@ -6,7 +6,7 @@ import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 function Map() {
     const navigate = useNavigate()
     const [searchParams, setSearchParams] = useSearchParams();
-    const [mapPosition, setMapPosition] = useState([40,21])
+    const [mapPosition, setMapPosition] = useState([40,0])
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
 
@@ -15,10 +15,10 @@ function Map() {
         <div className={styles.mapContainer} onClick={() => {
             navigate("form");
         }}>
-            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map}>
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
                 <Marker position={mapPosition}>
                     <Popup>
